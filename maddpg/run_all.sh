e1ca0ed632bf492732a469bad4667bfd8b6c4387
Ben Black <weepingwillowben@gmail.com>
2020-10-06 19:49:58 -0400

    made maddpg hyperparameter changes

--- maddpg/run_all.sh
+++ maddpg/run_all.sh
@@ -1,3 +1,3 @@
-python train_sisl.py --gamma=0.99 --lr=0.0001 --batch-size=512 --num-envs=8 --num-cpus=4 --scenario=waterworld
-python train_sisl.py --gamma=0.99 --lr=0.0001 --batch-size=512 --num-envs=8 --num-cpus=4 --scenario=pursuit
-python train_sisl.py --gamma=0.99 --lr=0.0001 --batch-size=512 --num-envs=8 --num-cpus=4 --scenario=multiwalker
+python train_sisl.py --gamma=0.99 --lr=0.0001 --batch-size=1024 --num-envs=64 --num-cpus=8 --scenario=waterworld
+python train_sisl.py --gamma=0.99 --lr=0.0001 --batch-size=1024 --num-envs=64 --num-cpus=8 --scenario=pursuit
+python train_sisl.py --gamma=0.99 --lr=0.0001 --batch-size=1024 --num-envs=64 --num-cpus=8 --scenario=multiwalker
